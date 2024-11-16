82a477b6ff22b1a9d79e0b05e7a8416256f92106
nicole-beatrice-go <119998974+nicole-beatrice-go@users.noreply.github.com>
2024-11-15 23:00:21 -0800

    Update CardForm.js

--- src/components/NewCard/CardForm.js
+++ src/components/NewCard/CardForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-
-import "./ExpenseForm.css";
+import "./CardForm.css";
 
 const CardForm = (props) => {
     const [enteredQuestion, setEnteredQuestion] = useState('');
@@ -26,32 +25,31 @@ const CardForm = (props) => {
         setEnteredAnswer('');
     };
 
-     
-  return (
-    <form onSubmit={submitHandler}>
-      <div className='new-card_allInputs'>
-        <div className='new-expense_input'>
-          <label>Question</label>
-          <input 
-            type='text' 
-            value={enteredTitle} 
-            onChange={questionChangeHandler} 
-          />
-        </div>
-        <div className='new-expense_input'>
-          <label>Answer</label>
-          <input 
-            type='text' 
-            value={enteredTitle}
-            onChange={answerChangeHandler}
-          />
-        </div>
-      </div>
-      <div className='new-card_actions'>
-        <button type='submit'>Add Card</button>
-      </div>
-    </form>
-  );
+    return (
+        <form onSubmit={submitHandler}>
+            <div className='new-card_allInputs'>
+                <div className='new-expense_input'>
+                    <label>Question</label>
+                    <input 
+                        type='text' 
+                        value={enteredQuestion} 
+                        onChange={questionChangeHandler} 
+                    />
+                </div>
+                <div className='new-expense_input'>
+                    <label>Answer</label>
+                    <input 
+                        type='text' 
+                        value={enteredAnswer}
+                        onChange={answerChangeHandler}
+                    />
+                </div>
+            </div>
+            <div className='new-card_actions'>
+                <button type='submit'>Add Card</button>
+            </div>
+        </form>
+    );
 };
 
-export default ExpenseForm;
+export default CardForm;
